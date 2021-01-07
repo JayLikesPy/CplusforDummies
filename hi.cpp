11a0653115d26675e6c87391cea02fe5cc9e707b
Jayro <68506471+JayLikesPy@users.noreply.github.com>
2021-01-07 11:13:13 -0500

    Add files via upload
    Gonna be updating this as the more i learn

---
+++ hi.cpp
@@ -0,0 +1,19 @@
+//So uh I decided to learn c++ so here this is. And no This isn't fucking skidded ffs just notes
+
+//when you make a C++ file there will always be a main "function"
+
+
+int main() // <- Main Function
+{
+    return 0, //<-- Output 
+}
+
+//Remeber when you execute your program it will always call the main function, The return is the output 4
+
+//Here we will try and print "Jay is cool"
+#include <iostream>
+int main ()
+{
+    std:: cout<<"jay Is cool\n";
+    return o, // The Return is pointless btw but it can stay
+}
